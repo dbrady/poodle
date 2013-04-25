4342b5616dcbaf7ad179c2eaa9abfe5b5fc3b27e
David Brady <dbrady@shinybit.com>
2013-04-24 18:40:27 -0600

    Backported some of the code to Ruby 1.9.x
    * Tee-hee, getting ahead of myself here... ;-) (I'm factoring up all
      this code under Ruby 1.8/1.9.x for a Ruby 2 talk)

--- Gemfile
+++ Gemfile
@@ -1,13 +1,29 @@
 source "http://rubygems.org"
 
-gem "prawn", "~>0.12.0"
+gem "prawn", "~>0.12"
 gem "trollop", "~>2.0"
 
 group :development do
-  gem "rb-fsevent", "~>0.9.0"
-  gem "guard", "~>1.0.1"
-  gem "guard-rspec", "~>0.6.0"
-  gem "growl", "~>1.0.3"
-  gem "rspec", "~>2.8"
   gem "debugger"
+  gem "guard", "~>1.0"
+  gem "growl", "~>1.0"
+
+  if RUBY_VERSION < '2.0.0'
+    gem "minitest", "~>4.0"
+    gem "rake", "~>10.0"
+  end
+
+  # ----------------------------------------------------------------------
+  # Guard File System listeners
+  #
+  # Kudos to the guard devs for knowing to only activate the FS
+  # listener that works for each given OS (Here linux, OSX, and
+  # Windows respectively)
+  gem 'rb-inotify', '~>0.9', :require => false
+  gem 'rb-fsevent', '~>0.9', :require => false
+  gem 'rb-fchange', '~>0.0', :require => false
+
+  # TODO: Post 1.9.3 this is more efficient on Windows than rb-fchange
+  # gem 'wdm', '~>0.1', :require => false
+  # ----------------------------------------------------------------------
 end
--- test/lib/test_date_patches.rb
+++ test/lib/test_date_patches.rb
@@ -5,20 +5,20 @@ class TestDatePatches < MiniTest::Unit::TestCase
   # from_wday is provided here merely as an executable comment so you
   # can see that beginning_of_workwook rewinds all days except Monday
   # to the previous Monday. (Mondays are returned unchanged.)
-  def assert_rewinds(from: '2012-03-12', from_wday: "Mon", to: '2012-03-12')
-    from = Date.parse from
-    to = Date.parse to
-    from_wday.must_equal from.strftime("%a")
+  def assert_rewinds(opts)
+    from = Date.parse opts[:from]
+    to = Date.parse opts[:to]
+    opts[:from_wday].must_equal from.strftime("%a")
     to.must_equal from.beginning_of_workweek
   end
 
   def test_rewinds_to_previous_monday
-    assert_rewinds from: '2013-04-22', from_wday: "Mon", to: '2013-04-22'
-    assert_rewinds from: '2013-04-23', from_wday: "Tue", to: '2013-04-22'
-    assert_rewinds from: '2013-04-24', from_wday: "Wed", to: '2013-04-22'
-    assert_rewinds from: '2013-04-25', from_wday: "Thu", to: '2013-04-22'
-    assert_rewinds from: '2013-04-26', from_wday: "Fri", to: '2013-04-22'
-    assert_rewinds from: '2013-04-27', from_wday: "Sat", to: '2013-04-22'
-    assert_rewinds from: '2013-04-28', from_wday: "Sun", to: '2013-04-22'
+    assert_rewinds :from => '2013-04-22', :from_wday => "Mon", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-23', :from_wday => "Tue", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-24', :from_wday => "Wed", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-25', :from_wday => "Thu", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-26', :from_wday => "Fri", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-27', :from_wday => "Sat", :to => '2013-04-22'
+    assert_rewinds :from => '2013-04-28', :from_wday => "Sun", :to => '2013-04-22'
   end
 end
